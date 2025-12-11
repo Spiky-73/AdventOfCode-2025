6d612047f8869688928151c02ea02e65c03c7eef
Spiky-73 <cocojeanne73@gmail.com>
2025-12-11 01:03:09 +0100

    day 9 part 2

--- src/day09_move_theater.rs
+++ src/day09_move_theater.rs
@@ -40,41 +40,38 @@ pub fn solve_p2<F: BufRead>(input: F) {
         })
         .collect();
 
-    let mut green_tiles: HashSet<(usize, usize)> = HashSet::new();
-
+    let mut lines: Vec<(usize, usize, usize, usize)> = Vec::new();
     for i in 0..red_tiles.len() {
-        let delta_x = red_tiles[i][0].abs_diff(red_tiles[(i + 1) % red_tiles.len()][0]);
-        let delta_y = red_tiles[i][1].abs_diff(red_tiles[(i + 1) % red_tiles.len()][1]);
-        let start_x = red_tiles[i][0].min(red_tiles[(i + 1) % red_tiles.len()][0]);
-        let start_y = red_tiles[i][1].min(red_tiles[(i + 1) % red_tiles.len()][1]);
-        for x in start_x..(start_x + delta_x + 1) {
-            for y in start_y..(start_y + delta_y + 1) {
-                green_tiles.insert((x, y));
-            }
-        }
+        lines.push((
+            red_tiles[i][0].min(red_tiles[(i + 1) % red_tiles.len()][0]),
+            red_tiles[i][1].min(red_tiles[(i + 1) % red_tiles.len()][1]),
+            red_tiles[i][0].max(red_tiles[(i + 1) % red_tiles.len()][0]),
+            red_tiles[i][1].max(red_tiles[(i + 1) % red_tiles.len()][1]),
+        ));
     }
 
     let mut max_area = 0;
 
     for i in 1..red_tiles.len() {
         'red_tile: for j in 0..i {
-            let delta_x = red_tiles[i][0].abs_diff(red_tiles[j][0]);
-            let delta_y = red_tiles[i][1].abs_diff(red_tiles[j][1]);
+            let left = red_tiles[i][0].min(red_tiles[j][0]);
+            let top = red_tiles[i][1].min(red_tiles[j][1]);
+            let right = red_tiles[i][0].max(red_tiles[j][0]);
+            let bot = red_tiles[i][1].max(red_tiles[j][1]);
 
-            let area = (delta_x + 1) * (delta_y + 1);
+            let area = (right - left + 1) * (bot - top + 1);
             if area <= max_area {
                 continue 'red_tile;
             }
-            let start_x = red_tiles[i][0].min(red_tiles[j][0]);
-            let start_y = red_tiles[i][1].min(red_tiles[j][1]);
 
-            for y in (start_y+1)..(start_y + delta_y) {
-                for x in (start_x+1)..(start_x + delta_x) {
-                    if green_tiles.contains(&(x, y)) {
-                        continue 'red_tile;
-                    }
+            for &(l, t, r, b) in lines.iter() {
+                let inter_w = ((right - 1).min(r) + 1).saturating_sub((left + 1).max(l));
+                let inter_h = ((bot - 1).min(b) + 1).saturating_sub((top + 1).max(t));
+                if inter_w != 0 && inter_h != 0 {
+                    continue 'red_tile;
                 }
             }
+
             max_area = area;
             println!(
                 "[({}, {}), ({}, {})]: {}",
